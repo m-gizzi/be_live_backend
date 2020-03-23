1f9d0cb175387bb11b18a860a6155fc67746e2d3
m-gizzi <matthewgizzi@verizon.net>
2020-03-23 13:24:01 -0500

    Seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Tag.all.destroy_all
+Event.all.destroy_all
+User.all.destroy_all
+
+admin = User.create(name: 'admin', email: 'admin@belive.com', img_url: '')
+
+Event.create(title: "Fuck off Coronavirus", start_date: DateTime.strptime('03/30/2020', '%m/%d/%Y'), end_date: DateTime.strptime('03/31/2020', '%m/%d/%Y'), ongoing: false, description: "Join us as we all shout fuck you together, but online.", private: false, url: "www.facebook.com", img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrTDhOIlFbP2CK2SqjG7w9QL0uB7S2iHau3talnbpizQrX9DD_', host_id: admin.id)
+Event.create(title: "Fuck off Coronavirus 2", start_date: DateTime.strptime('04/01/2020', '%m/%d/%Y'), end_date: DateTime.strptime('04/10/2020', '%m/%d/%Y'), ongoing: false, description: "Join us as we all shout fuck you together, but online.", private: false, url: "www.facebook.com", img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrTDhOIlFbP2CK2SqjG7w9QL0uB7S2iHau3talnbpizQrX9DD_', host_id: admin.id)
+Event.create(title: "Fuck off Coronavirus 3", start_date: DateTime.strptime('04/06/2020', '%m/%d/%Y'), end_date: DateTime.strptime('04/20/2020', '%m/%d/%Y'), ongoing: false, description: "Join us as we all shout fuck you together, but online.", private: false, url: "www.facebook.com", img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrTDhOIlFbP2CK2SqjG7w9QL0uB7S2iHau3talnbpizQrX9DD_', host_id: admin.id)
+
+Tag.create(tag_name: "Party")
