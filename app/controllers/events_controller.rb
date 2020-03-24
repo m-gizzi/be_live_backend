2676046d86d7f859ead5e87c24c2c5edcb6c2b69
m-gizzi <matthewgizzi@verizon.net>
2020-03-24 17:13:44 -0500

    Backend for new event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,16 @@ class EventsController < ApplicationController
 		render json: EventSerializer.new(@events)
 	end
 
+	def create
+		Event.create(event_params)
+		events = Event.all
+		render json: EventSerializer.new(events)
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:title, :start_date, :end_date, :ongoing, :description, :private, :url, :img_url, :host_id)
+	end
+
 end
