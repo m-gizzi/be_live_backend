45e281a76079df4242a0f4faa359fc90dab672a7
m-gizzi <matthewgizzi@verizon.net>
2020-03-25 12:02:58 -0500

    Backend search responsiveness

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,13 @@
 class EventSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :start_date, :end_date, :ongoing, :description, :private, :url, :img_url, :host_id
-  has_many :tags, through: :eventsTags
-  has_many :users, through: :rsvps
+  attributes :title, :start_date, :end_date, :ongoing, :description, :private, :url, :img_url, :host_id, :tags, :users
+
+  def tags
+    @object.tags
+  end
+
+  def users
+    @object.users
+  end
+
 end
